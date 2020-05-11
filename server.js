01f5de1fbdbc7e1bda7cd14879b262d556a0c024
Kauri Nanda <bknanda6@gmail.com>
2020-05-10 22:11:57 -0600

    added procfile and gitignore file

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const util = require("util");
 
 // Express App and PORT
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 // Express Middleware Functions
 app.use(express.urlencoded({ extended: true }));
